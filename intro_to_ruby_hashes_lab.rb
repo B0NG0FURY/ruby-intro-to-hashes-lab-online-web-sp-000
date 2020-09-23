24fd6dbf484f3792a10893a17d42a8c99d21126a
John Troutman <jtroutmanman@gmail.com>
2020-09-23 16:59:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,12 +20,7 @@ def monopoly_with_second_tier
   monopoly = {
 	  railroads: {
 	    pieces: 4,
-	    names: {
-	      reading_railroad: {},
-	      pennsylvania_railroad: {},
-	      b_and_o_railroad: {},
-	      shortline: {}
-	    },
+	    names: {},
 	    rent_in_dollars: {
 	      one_piece_owned: 25,
 	      two_pieces_owned: 50,
@@ -42,7 +37,10 @@ def monopoly_with_third_tier
 	  railroads: {
 	    pieces: 4,
 	    names: {
-	      
+	      reading_railroad: {},
+	      pennsylvania_railroad: {},
+	      b_and_o_railroad: {},
+	      shortline: {}
 	    },
 	    rent_in_dollars: {}
 	  }
